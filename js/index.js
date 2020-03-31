17dfe944a993bad064c2eff43bcfa54a362131a3
Chad Hinds <chadrickj8@gmail.com>
2020-03-30 20:46:52 -0400

    change driver name

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 //names and inputs
 
-let hacker1 = "Willllllllll";
+let hacker1 = "Devin";
 console.log(`The drivers name is ${hacker1}`);
 
-let hacker2 = "Someone else";
+let hacker2 = "Widowmaker";
 console.log(`The navigators name is ${hacker2}`);
 
 //conditionals
-
 if (hacker1.length > hacker2.length) {
     console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
 }
@@ -17,8 +16,6 @@ else if (hacker1.length < hacker2.length) {
 else if (hacker1.length === hacker2.length) {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 };
-
-//loops
 
 // //loops
 // console.log('niko'.toUpperCase())
@@ -35,30 +32,39 @@ let newHacker = "";
 for (let i = 0; i < hacker1.length; i++) {
     newHacker = newHacker + (hacker1.charAt(i) + " ")
     // newHacker.charAt(i) + " ";
-
 }
+
 console.log(newHacker.toUpperCase());
 
 let newWord = newHacker.toUpperCase();
 
-newWord = newWord.split("").reverse().join("");
-
+newWord = hacker2.split("").reverse().join("");
 
 console.log(newWord);
 
+const wordArray = [hacker1, hacker2];
 
-// if (hacker1 > hacker2) {
-//     console.log('The driver´s name goes first')
-// } else if (hacker1 < hacker2) {
-//     console.log('The navigator goes first definitely')
-// } else {
-//     console.log('What?! You both got the same name?')
-// } 
-​
-/ for (let letter in hackerArray) {
-//   console.log(letter.toUpperCase());
-// }
-​
-/ let nameOrder = "";
-// for (i = hacker1.length -1; i >= 0; i--) {
-//     nameOrder += hacker1[i];
+// Lexicographic order!
+wordArray.sort()
+
+if (wordArray[0] === hacker1) {
+    console.log(`The driver's name goes first.`)
+}
+
+else if (wordArray[0] === hacker2) {
+    console.log(`Yo, the navigator goes first definitely.`)
+}
+
+else {
+    console.log(`What?! You both have the same name?`)
+}
+
+//BONUS
+const ipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra augue vitae consequat tristique. Vivamus a lectus maximus, vestibulum est vitae, finibus erat. Suspendisse neque felis, imperdiet id varius in, convallis sed nulla. Proin sed elit augue. Phasellus volutpat pellentesque est. Ut quis ornare leo. Donec pellentesque est justo, ut ultrices erat rhoncus in. Maecenas id rhoncus dolor. Donec nec urna metus. Cras dolor leo, aliquam ut consequat at, dictum nec erat. Sed tristique ullamcorper metus, quis molestie neque mattis vitae.
+
+Nam sed molestie ipsum, in dictum lacus. Aenean ante diam, porta et consectetur ac, ornare eu nisi. Mauris eu lacinia purus. Aenean fringilla convallis nulla id venenatis. Phasellus quis ipsum ligula. Donec a ipsum ut tellus aliquam ullamcorper sed tristique magna. Proin malesuada felis nec ante pharetra egestas. Donec purus dui, tincidunt vel velit suscipit, placerat suscipit ligula.
+
+In hac habitasse platea dictumst. Etiam efficitur nulla iaculis dui faucibus accumsan. Mauris imperdiet commodo ipsum sed vestibulum. Vivamus arcu odio, vulputate in luctus nec, viverra ac felis. Proin quis orci sed ex tincidunt consectetur. Suspendisse hendrerit sit amet purus quis consectetur. Nulla faucibus condimentum leo et laoreet. Integer finibus orci et mi vehicula, at aliquam neque ultricies.`
+// number of words. A lot of split action :)
+console.log(ipsum.split(' ').length);
+
